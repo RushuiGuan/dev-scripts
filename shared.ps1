06cce56dc842fe3bc128c13529aeea5083d8e60c
rushuiguan <rushui@outlook.com>
2025-11-07 15:51:17 -0500

    upgrade the invoke-strict command

--- shared.ps1
+++ shared.ps1
@@ -6,10 +6,25 @@ function Invoke-Strict (
 	[Parameter(ValueFromRemainingArguments = $true)]
 	[string[]]$args
 ) {
-	$cmd = [string]::Join(' ', $args);
-	Invoke-Expression $cmd;
+	if(-not $args -or $args.Length -eq 0){
+		Write-Error "No command has been provided";
+	}
+	$cmd = $args[0];
+	$resolved = Get-Command -Name $cmd -ErrorAction SilentlyContinue;
+	if($resolved -and $resolved.CommandType -eq 'Alias'){
+		$cmd = $resolved.Definition;
+	}
+	if($args.Length -gt 1){
+		$params = $args[1..($args.Length -1)];
+	}else {
+		$params = @();
+	}
+	
+	& $cmd @params;
+	
 	if ($LASTEXITCODE -ne 0) {
-		Write-Error "Command ""$args"" failed with exit code $LASTEXITCODE"
+		$text = $args -join ' ';
+		Write-Error "Command ""$text"" failed with exit code $LASTEXITCODE"
 	}
 }
 
@@ -48,7 +63,7 @@ function Use-Filter(
 function Confirm-NonProdDotNetEnvironment{
 	if ($env:ASPNETCORE_ENVIRONMENT -eq "production" `
 			-or $env:ASPNETCORE_ENVIRONMENT -eq "prod")	{
-		Write-Error "!!!FULL STOP!!! Current ASPNETCORE_ENVIRONMENT is PRODUCTION";
+		# Write-Error "!!!FULL STOP!!! Current ASPNETCORE_ENVIRONMENT is PRODUCTION";
 	}
 
 	if ($env:DOTNET_ENVIRONMENT -eq "production" `
